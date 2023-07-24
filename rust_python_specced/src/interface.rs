563a1e30f9a6de5d82d9af2aa253d2b98571ce86
mimo31 <viktor.fukala@gmail.com>
2023-07-24 13:31:17 +0200

    Progress

--- rust_python_specced/src/interface.rs
+++ rust_python_specced/src/interface.rs
@@ -1,146 +1,287 @@
 use prusti_contracts::*;
 
-pub type Py_ssize_t = isize;
+pub type pyt_ssize_t = isize;
 
 #[repr(C)]
 #[derive(Clone, Copy, PartialEq, Eq)]
-pub struct PyObjectPointer {
-    _private: usize
+pub struct PytObjectPointer {
+    pub _private: usize
+}
+
+type GpyErrorState = bool;
+
+pub struct GpyGlobalState {
+    _private: usize, // this represents opaque data (the heap of PyObjects)
+    error: GpyErrorState,
+    constants: GpyConstantObjects,
+}
+
+struct GpyConstantObjects {
+    py_none: PytObjectPointer,
+    py_false: PytObjectPointer,
+    py_true: PytObjectPointer,
+    py_not_implemented: PytObjectPointer,
+}
+
+predicate! {
+    fn has_value_and_pos_ref_count(obj: PytObjectPointer, data: GpyObjectData, s: &GpyGlobalState) -> bool {
+        (obj.gpy_get(s).data === data) & (obj.gpy_get(s).ref_count >= 1)
+    }
+}
+
+#[trusted]
+#[ensures(!result.error)]
+#[ensures(has_value_and_pos_ref_count(result.constants.py_none, GpyObjectData::PyNone, &result))]
+#[ensures(has_value_and_pos_ref_count(result.constants.py_false, GpyObjectData::PyBool(false), &result))]
+#[ensures(has_value_and_pos_ref_count(result.constants.py_none, GpyObjectData::PyBool(true), &result))]
+#[ensures(has_value_and_pos_ref_count(result.constants.py_none, GpyObjectData::PyNotImplemented, &result))]
+pub fn gpy_create_state() -> GpyGlobalState {
+    GpyGlobalState {
+        _private: 0,
+        error: false,
+        constants: GpyConstantObjects {
+            py_none: PytObjectPointer { _private: 0 },
+            py_false: PytObjectPointer { _private: 0 },
+            py_true: PytObjectPointer { _private: 0 },
+            py_not_implemented: PytObjectPointer { _private: 0 },
+        }
+    }
 }
 
 obligation! {
-    fn py_ref_held(amount: usize, obj: PyObjectPointer);
+    pub fn gpy_ref_held(amount: usize, obj: PytObjectPointer);
 }
 
 obligation! {
-    fn py_initialized(amount: usize);
+    fn gpy_initialized(amount: usize);
 }
 
-impl PyObjectPointer {
+impl PytObjectPointer {
     #[pure]
     pub fn is_null(&self) -> bool {
         self._private == 0
     }
+
+    // spec-only
+    #[trusted]
+    #[pure]
+    pub fn gpy_get(&self, s: &GpyGlobalState) -> GpyObject {
+        unreachable!()
+    }
+}
+
+#[derive(Clone, Copy)]
+struct GpyObject {
+    ref_count: pyt_ssize_t,
+    data: GpyObjectData,
+    typ: PytObjectPointer,
+}
+
+#[derive(Clone, Copy)]
+enum GpyObjectData {
+    PyNone,
+    PyLong,
+    PyBool(bool),
+    PyDict,
+    PyNotImplemented,
+    PyType(GpyType),
+}
+
+impl GpyObjectData {
+    #[pure]
+    fn is_bool(&self) -> bool {
+        matches!(self, GpyObjectData::PyBool(_))
+    }
+}
+
+#[derive(Clone, Copy)]
+struct GpyType {
+    getattr: Option<dyn GpyGetattr>,
+    setattr: Option<dyn GpySetattr>,
+}
+
+trait GpyGetattr {
+    // returns the result and the updated error state
+    #[pure]
+    fn getattr(obj: PytObjectPointer, name: PytObjectPointer, s: &GpyGlobalState) -> (PytObjectPointer, GpyErrorState);
+}
+
+trait GpySetattr {
+    // returns the updated object and the updated error state
+    #[pure]
+    fn setattr(obj: PytObjectPointer, name: PytObjectPointer, s: &GpyGlobalState) -> (GpyObjectData, GpyErrorState);
 }
 
 extern "C" {
-    pub fn Py_Initialize();
-    pub fn Py_Finalize();
-    fn Py_IncRef(obj: PyObjectPointer);
-    fn Py_DecRef(obj: PyObjectPointer);
-    fn Py_REFCNT(obj: PyObjectPointer) -> Py_ssize_t;
-
-    fn PyLong_AsLong(obj: PyObjectPointer) -> libc::c_long;
-    fn PyLong_FromLong(v: libc::c_long) -> PyObjectPointer;
-    fn MyLong_CheckExact(obj: PyObjectPointer) -> libc::c_int;
-
-    fn PyDict_New() -> PyObjectPointer;
-    fn MyDict_CheckExact(obj: PyObjectPointer) -> libc::c_int;
-
-    fn MyNone_GetNoIncRef() -> PyObjectPointer;
-    fn MyBool_GetTrueNoIncRef() -> PyObjectPointer;
-    fn MyBool_GetFalseNoIncRef() -> PyObjectPointer;
-
-    fn MyBool_Check(obj: PyObjectPointer) -> libc::c_int;
-
-    fn My_IsNone(obj: PyObjectPointer) -> libc::c_int;
-    fn My_IsTrue(obj: PyObjectPointer) -> libc::c_int;
-    fn My_IsFalse(obj: PyObjectPointer) -> libc::c_int;
-    pub fn My_Is(obj0: PyObjectPointer, obj1: PyObjectPointer) -> libc::c_int;
-}
-
-#[extern_spec(crate)]
-extern "C" {
-    #[ensures(py_initialized(1))]
     fn Py_Initialize();
-    #[requires(py_initialized(1))]
     fn Py_Finalize();
+    fn Py_IncRef(obj: PytObjectPointer);
+    fn Py_DecRef(obj: PytObjectPointer);
+    fn Py_REFCNT(obj: PytObjectPointer) -> pyt_ssize_t;
+
+    fn PyLong_AsLong(obj: PytObjectPointer) -> libc::c_long;
+    fn PyLong_FromLong(v: libc::c_long) -> PytObjectPointer;
+    fn MyLong_CheckExact(obj: PytObjectPointer) -> libc::c_int;
+
+    fn PyDict_New() -> PytObjectPointer;
+    fn MyDict_CheckExact(obj: PytObjectPointer) -> libc::c_int;
+
+    fn MyNone_GetNoIncRef() -> PytObjectPointer;
+    fn MyBool_GetTrueNoIncRef() -> PytObjectPointer;
+    fn MyBool_GetFalseNoIncRef() -> PytObjectPointer;
+
+    fn MyBool_Check(obj: PytObjectPointer) -> libc::c_int;
+
+    fn My_IsNone(obj: PytObjectPointer) -> libc::c_int;
+    fn My_IsTrue(obj: PytObjectPointer) -> libc::c_int;
+    fn My_IsFalse(obj: PytObjectPointer) -> libc::c_int;
+    fn My_Is(obj0: PytObjectPointer, obj1: PytObjectPointer) -> libc::c_int;
+
+    fn PyObject_HasAttr(obj: PytObjectPointer, name: PytObjectPointer) -> libc::c_int;
+}
+
+predicate! {
+    fn all_other_preserved(changed: PytObjectPointer, s0: &GpyGlobalState, s: &GpyGlobalState) -> bool {
+        forall(|q: PytObjectPointer| (q !== changed ==> q.gpy_get(s) === q.gpy_get(s0)))
+    }
+}
+
+predicate! {
+    fn all_objects_preserved(s0: &GpyGlobalState, s: &GpyGlobalState) -> bool {
+        forall(|q: PytObjectPointer| q.gpy_get(s) === q.gpy_get(s0))
+    }
+}
+
+predicate! {
+    fn errors_preserved(s0: &GpyGlobalState, s: &GpyGlobalState) -> bool {
+        s0.error == s.error
+    }
+}
+
+predicate! {
+    fn constants_preserved(s0: &GpyGlobalState, s: &GpyGlobalState) -> bool {
+        s0.constants === s.constants
+    }
 }
 
 #[trusted]
-#[requires(py_initialized(1))]
-#[requires(py_ref_held(1, obj))]
-#[ensures(py_initialized(1))]
-#[ensures(py_ref_held(2, obj))]
-pub unsafe fn pyt_incref(obj: PyObjectPointer) {
+#[ensures(gpy_initialized(1))]
+pub unsafe fn pyt_initialize() {
+    Py_Initialize();
+}
+
+#[trusted]
+#[requires(gpy_initialized(1))]
+pub unsafe fn pyt_finalize() {
+    Py_Finalize();
+}
+
+#[trusted]
+#[requires(gpy_initialized(1))]
+#[requires(gpy_ref_held(1, obj))]
+#[ensures(gpy_initialized(1))]
+#[ensures(gpy_ref_held(2, obj))]
+#[ensures(obj.gpy_get(old(s)).ref_count + 1 == obj.gpy_get(s).ref_count)]
+#[ensures(all_other_preserved(obj, old(s), s))]
+#[ensures(errors_preserved(old(s), s))]
+#[ensures(constants_preserved(old(s), s))]
+pub unsafe fn pyt_incref(obj: PytObjectPointer, s: &mut GpyGlobalState) {
     Py_IncRef(obj)
 }
 
 #[trusted]
-#[requires(py_initialized(1))]
-#[requires(py_ref_held(1, obj))]
-#[ensures(py_initialized(1))]
-pub unsafe fn pyt_decref(obj: PyObjectPointer) {
+#[requires(gpy_initialized(1))]
+#[requires(gpy_ref_held(1, obj))]
+#[ensures(gpy_initialized(1))]
+#[ensures(obj.gpy_get(old(s)).ref_count - 1 == obj.gpy_get(s).ref_count)]
+#[ensures(all_other_preserved(obj, old(s), s))]
+#[ensures(errors_preserved(old(s), s))]
+#[ensures(constants_preserved(old(s), s))]
+pub unsafe fn pyt_decref(obj: PytObjectPointer, s: &mut GpyGlobalState) {
     Py_DecRef(obj)
 }
 
 #[trusted]
-#[requires(py_initialized(1))]
-#[requires(py_ref_held(1, obj))]
-#[ensures(py_initialized(1))]
-#[ensures(py_ref_held(1, obj))]
-pub unsafe fn pyt_refcnt(obj: PyObjectPointer) -> Py_ssize_t {
+#[pure]
+#[requires(gpy_initialized(1))]
+#[requires(gpy_ref_held(1, obj))]
+#[ensures(result == obj.gpy_get(s).ref_count)]
+pub unsafe fn pyt_refcnt(obj: PytObjectPointer, s: &GpyGlobalState) -> pyt_ssize_t {
     Py_REFCNT(obj)
 }
 
 #[trusted]
-#[requires(py_initialized(1))]
-#[requires(py_ref_held(1, obj))]
-#[ensures(py_initialized(1))]
-#[ensures(py_ref_held(1, obj))]
-pub unsafe fn pytlong_aslong(obj: PyObjectPointer) -> libc::c_long {
-    // TODO this might throw an error!!
+#[requires(gpy_initialized(1))]
+#[requires(gpy_ref_held(1, obj))]
+#[ensures(gpy_initialized(1))]
+#[ensures(gpy_ref_held(1, obj))]
+#[ensures(all_objects_preserved(old(s), s))]
+#[ensures(constants_preserved(old(s), s))]
+pub unsafe fn pytlong_aslong(obj: PytObjectPointer, s: &mut GpyGlobalState) -> libc::c_long {
     PyLong_AsLong(obj)
 }
 
 #[trusted]
-#[requires(py_initialized(1))]
-#[ensures(py_initialized(1))]
-#[ensures(!result.is_null() ==> py_ref_held(1, result))]
-pub unsafe fn pytlong_fromlong(v: libc::c_long) -> PyObjectPointer {
+#[requires(gpy_initialized(1))]
+#[ensures(gpy_initialized(1))]
+#[ensures(!result.is_null() ==> gpy_ref_held(1, result) & all_other_preserved(result, old(s), s) & (result.gpy_get(s).ref_count == result.gpy_get(old(s)).ref_count + 1) & (result.gpy_get(s).data === GpyObjectData::PyLong))]
+#[ensures(result.is_null() ==> all_objects_preserved(old(s), s))]
+#[ensures(result.is_null() ==> s.error)]
+#[ensures(!result.is_null() ==> errors_preserved(old(s), s))]
+#[ensures(constants_preserved(old(s), s))]
+pub unsafe fn pytlong_fromlong(v: libc::c_long, s: &mut GpyGlobalState) -> PytObjectPointer {
     PyLong_FromLong(v)
 }
 
 
 #[trusted]
-#[requires(py_initialized(1))]
-#[requires(py_ref_held(1, obj))]
-#[ensures(py_initialized(1))]
-#[ensures(py_ref_held(1, obj))]
-pub unsafe fn pylong_checkexact(obj: PyObjectPointer) -> bool {
+#[pure]
+#[requires(gpy_initialized(1))]
+#[requires(gpy_ref_held(1, obj))]
+#[ensures(result <==> obj.gpy_get(s).data === GpyObjectData::PyLong)]
+pub unsafe fn pylong_checkexact(obj: PytObjectPointer, s: &GpyGlobalState) -> bool {
     MyLong_CheckExact(obj) != 0
 }
 
 #[trusted]
-#[requires(py_initialized(1))]
-#[ensures(py_initialized(1))]
-#[ensures(!result.is_null() ==> py_ref_held(1, result))]
-pub unsafe fn pytdict_new() -> PyObjectPointer {
+#[requires(gpy_initialized(1))]
+#[ensures(gpy_initialized(1))]
+#[ensures(!result.is_null() ==> gpy_ref_held(1, result) & (result.gpy_get(old(s)).ref_count == 0) & (result.gpy_get(s).data === GpyObjectData::PyDict) & (result.gpy_get(s).ref_count == 1))]
+#[ensures(result.is_null() ==> all_objects_preserved(old(s), s))]
+#[ensures(result.is_null() ==> s.error)]
+#[ensures(!result.is_null() ==> errors_preserved(old(s), s))]
+#[ensures(constants_preserved(old(s), s))]
+pub unsafe fn pytdict_new(s: &mut GpyGlobalState) -> PytObjectPointer {
     PyDict_New()
 }
 
 #[trusted]
-#[requires(py_initialized(1))]
-#[requires(py_ref_held(1, obj))]
-#[ensures(py_initialized(1))]
-#[ensures(py_ref_held(1, obj))]
-pub unsafe fn pytdict_checkexact(obj: PyObjectPointer) -> bool {
+#[pure]
+#[requires(gpy_initialized(1))]
+#[requires(gpy_ref_held(1, obj))]
+#[ensures(result <==> obj.gpy_get(s).data === GpyObjectData::PyDict)]
+pub unsafe fn pytdict_checkexact(obj: PytObjectPointer, s: &GpyGlobalState) -> bool {
     MyDict_CheckExact(obj) != 0
 }
 
 #[trusted]
 #[pure]
-// TODO require py_initialized
-pub unsafe fn pytnone_getnoincref() -> PyObjectPointer {
+#[requires(gpy_initialized(1))]
+#[ensures(result === s.constants.py_none)]
+pub unsafe fn pytnone_getnoincref(s: &GpyGlobalState) -> PytObjectPointer {
     MyNone_GetNoIncRef()
 }
 
 #[trusted]
-#[requires(py_initialized(1))]
-#[ensures(py_initialized(1))]
-#[ensures(result == unsafe { pytnone_getnoincref() })]
-#[ensures(py_ref_held(1, result))]
-pub unsafe fn pytnone_get() -> PyObjectPointer {
+#[requires(gpy_initialized(1))]
+#[ensures(gpy_initialized(1))]
+#[ensures(gpy_ref_held(1, result))]
+#[ensures(result === s.constants.py_none)]
+#[ensures(constants_preserved(old(s), s))]
+#[ensures(all_other_preserved(s.constants.py_none, old(s), s))]
+#[ensures(s.constants.py_none.gpy_get(s).data === s.constants.py_none.gpy_get(old(s)).data)]
+#[ensures(s.constants.py_none.gpy_get(s).ref_count == s.constants.py_none.gpy_get(old(s)).ref_count + 1)]
+pub unsafe fn pytnone_get(s: &mut GpyGlobalState) -> PytObjectPointer {
     let obj = MyNone_GetNoIncRef();
     Py_IncRef(obj);
     obj
@@ -148,17 +289,22 @@ pub unsafe fn pytnone_get() -> PyObjectPointer {
 
 #[trusted]
 #[pure]
-// TODO require py_initialized
-pub unsafe fn pytbool_gettruenoincref() -> PyObjectPointer {
+#[requires(gpy_initialized(1))]
+#[ensures(result === s.constants.py_true)]
+pub unsafe fn pytbool_gettruenoincref(s: &GpyGlobalState) -> PytObjectPointer {
     MyBool_GetTrueNoIncRef()
 }
 
 #[trusted]
-#[requires(py_initialized(1))]
-#[ensures(py_initialized(1))]
-#[ensures(result == unsafe { pytbool_gettruenoincref() })]
-#[ensures(py_ref_held(1, result))]
-pub unsafe fn pytbool_gettrue() -> PyObjectPointer {
+#[requires(gpy_initialized(1))]
+#[ensures(gpy_initialized(1))]
+#[ensures(gpy_ref_held(1, result))]
+#[ensures(result === s.constants.py_true)]
+#[ensures(constants_preserved(old(s), s))]
+#[ensures(all_other_preserved(s.constants.py_true, old(s), s))]
+#[ensures(s.constants.py_true.gpy_get(s).data === s.constants.py_true.gpy_get(old(s)).data)]
+#[ensures(s.constants.py_true.gpy_get(s).ref_count == s.constants.py_true.gpy_get(old(s)).ref_count + 1)]
+pub unsafe fn pytbool_gettrue(s: &GpyGlobalState) -> PytObjectPointer {
     let obj = MyBool_GetTrueNoIncRef();
     Py_IncRef(obj);
     obj
@@ -166,17 +312,22 @@ pub unsafe fn pytbool_gettrue() -> PyObjectPointer {
 
 #[trusted]
 #[pure]
-// TODO require py_initialized
-pub unsafe fn pytbool_getfalsenoincref() -> PyObjectPointer {
+#[requires(gpy_initialized(1))]
+#[ensures(result === s.constants.py_false)]
+pub unsafe fn pytbool_getfalsenoincref(s: &GpyGlobalState) -> PytObjectPointer {
     MyBool_GetFalseNoIncRef()
 }
 
 #[trusted]
-#[requires(py_initialized(1))]
-#[ensures(py_initialized(1))]
-#[ensures(result == unsafe { pytbool_getfalsenoincref() })]
-#[ensures(py_ref_held(1, result))]
-pub unsafe fn pytbool_getfalse() -> PyObjectPointer {
+#[requires(gpy_initialized(1))]
+#[ensures(gpy_initialized(1))]
+#[ensures(gpy_ref_held(1, result))]
+#[ensures(result === s.constants.py_false)]
+#[ensures(constants_preserved(old(s), s))]
+#[ensures(all_other_preserved(s.constants.py_false, old(s), s))]
+#[ensures(s.constants.py_false.gpy_get(s).data === s.constants.py_false.gpy_get(old(s)).data)]
+#[ensures(s.constants.py_false.gpy_get(s).ref_count == s.constants.py_false.gpy_get(old(s)).ref_count + 1)]
+pub unsafe fn pytbool_getfalse(s: &GpyGlobalState) -> PytObjectPointer {
     let obj = MyBool_GetFalseNoIncRef();
     Py_IncRef(obj);
     obj
@@ -184,43 +335,45 @@ pub unsafe fn pytbool_getfalse() -> PyObjectPointer {
 
 #[trusted]
 #[pure]
-// require ref held (?)
-#[ensures(result <==> unsafe { (pyt_istrue(obj) || pyt_isfalse(obj)) })]
-// TODO require py_initialized
-pub unsafe fn pytbool_check(obj: PyObjectPointer) -> bool {
+#[requires(gpy_initialized(1))]
+#[requires(gpy_ref_held(1, obj))]
+#[ensures(result == obj.gpy_get(s).data.is_bool())]
+pub unsafe fn pytbool_check(obj: PytObjectPointer, s: &GpyGlobalState) -> bool {
     MyBool_Check(obj) != 0
 }
 
 #[trusted]
-// require ref held (?)
 #[pure]
-#[ensures(result <==> obj === unsafe { pytnone_getnoincref() })]
-// TODO require py_initialized
-pub unsafe fn pyt_isnone(obj: PyObjectPointer) -> bool {
+#[requires(gpy_initialized(1))]
+#[ensures(result <==> obj == s.constants.py_none)]
+pub unsafe fn pyt_isnone(obj: PytObjectPointer, s: &GpyGlobalState) -> bool {
     My_IsNone(obj) != 0
 }
 
 #[trusted]
-// require ref held (?)
 #[pure]
-#[ensures(result <==> obj === unsafe { pytbool_gettruenoincref() })]
-// TODO require py_initialized
-pub unsafe fn pyt_istrue(obj: PyObjectPointer) -> bool {
+#[requires(gpy_initialized(1))]
+#[ensures(result <==> obj == s.constants.py_true)]
+pub unsafe fn pyt_istrue(obj: PytObjectPointer, s: &GpyGlobalState) -> bool {
     My_IsTrue(obj) != 0
 }
 
 #[trusted]
-// require ref held (?)
 #[pure]
-#[ensures(result <==> obj === unsafe { pytbool_gettruenoincref() })]
-// TODO require py_initialized
-pub unsafe fn pyt_isfalse(obj: PyObjectPointer) -> bool {
+#[requires(gpy_initialized(1))]
+#[ensures(result <==> obj == s.constants.py_false)]
+pub unsafe fn pyt_isfalse(obj: PytObjectPointer, s: &GpyGlobalState) -> bool {
     My_IsFalse(obj) != 0
 }
 
-#[extern_spec(crate)]
-extern "C" {
-    #[pure]
-    #[ensures(result != 0 <==> obj0 === obj1)]
-    fn My_Is(obj0: PyObjectPointer, obj1: PyObjectPointer) -> libc::c_int;
+#[trusted]
+#[pure]
+#[requires(gpy_initialized(1))]
+#[ensures(result <==> obj0 == obj1)]
+pub unsafe fn pyt_is(obj0: PytObjectPointer, obj1: PytObjectPointer) -> bool {
+    My_Is(obj0, obj1) != 0
+}
+
+pub unsafe fn pytobject_has_attr(obj0: PytObjectPointer, obj1: PytObjectPointer) -> bool {
+
 }
--- rust_python_specced/src/main.rs
+++ rust_python_specced/src/main.rs
@@ -10,6 +10,19 @@ fn pri(v: libc::c_long) {
 }
 
 fn main() {
+    //prusti_inhale!(gpy_ref_held(1, PytObjectPointer { _private: 0 }));
+    //prusti_exhale!(gpy_ref_held(1, PytObjectPointer { _private: 0 }));
+    unsafe {
+        pyt_initialize();
+        let mut s = gpy_create_state();
+        let lo = pytlong_fromlong(33, &mut s);
+        if !lo.is_null() {
+            pyt_decref(lo, &mut s);
+        }
+        prusti_refute!(false);
+        pyt_finalize();
+    }
+    /*
     unsafe {
         Py_Initialize();
         let lo = pytlong_fromlong(33);
@@ -24,4 +37,5 @@ fn main() {
         }
         Py_Finalize();
     }
+    */
 }
